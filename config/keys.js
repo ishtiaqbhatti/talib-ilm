562bcb46805b3e8ab676af7f0779a99858592317
Ishtiaq Bhatti <Ishtiaqbhatti455@gmail.com>
2018-09-09 14:19:26 -0700

    First commit ever

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./keys_prod');
+// } else {
+//   module.exports = require('./keys_dev');
+// }
+module.exports = {
+  mongoURI: 'mongodb://ishtiaq:ishtiaq123@ds251002.mlab.com:51002/talib-ilm' 
+};
+
